af7d4baa9ee318e1ca77541fbda9ccba4d8010fa
TJaiwanth3 <jaiwanthtatuskar2@gmail.com>
2025-03-10 16:28:41 +0530

    Add a new line in main

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Added a new line   \n";
   return 0;
 }
